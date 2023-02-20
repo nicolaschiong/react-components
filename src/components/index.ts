326e2291fa9d56a547f4bf874d76a5142e4800b0
Nick Chiong <nchiong@labsavvy.com>
2023-02-20 17:49:25 +0800

    add component import

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import { ReloadButton } from "./Button/ReloadButton/ReloadButton"
+import { LoaderButton } from "./Button/LoaderButton/LoaderButton"
+import { DeleteButton } from "./Button/DeleteButton/DeleteButton"
+
+export { ReloadButton, LoaderButton, DeleteButton }
